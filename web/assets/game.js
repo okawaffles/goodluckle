91dc9e4feeed1fba62e8ff69e125529d0909a24c
Millie Aurora <oka@waffle.moe>
2025-07-06 21:42:41 -0400

    ok this works i think

--- web/assets/game.js
+++ web/assets/game.js
@@ -147,7 +147,7 @@ function start() {
     };
 
     document.getElementById('keyboard').oninput = function() {
-        alert('input!');
+        // alert('input!');
         if (document.getElementById('keyboard').value.length < last_mobile_input_count) {
             // remove letter
             game_state.current_letter--;
@@ -155,7 +155,7 @@ function start() {
             last_mobile_input_count = document.getElementById('keyboard').value.length;
         } else if (document.getElementById('keyboard').value.length > last_mobile_input_count) {
             // add letter
-            game_state.letters[game_state.current_letter] = document.getElementById('keyboard').value[game_state.current_letter];
+            game_state.letters[game_state.current_letter] = document.getElementById('keyboard').value[game_state.current_letter].toUpperCase();
             game_state.current_letter++;
             last_mobile_input_count = document.getElementById('keyboard').value.length;
         }
